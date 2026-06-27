4bea5eb33c2cbdf56714643275fa763bb3e99705
jhxnnat <jhonadav@tutamail.com>
2026-06-26 23:27:05 -0500

    new shiny stuff

---
+++ code/bin/executable_launch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# source: https://wiki.alpinelinux.org/wiki/D-Bus#D-Bus_session_bus
+if [ ! -e "/tmp/dbus-$USER-env" ]; then
+       echo "Creating new dbus session on /tmp/dbus-$USER-env"
+       export $(dbus-launch)
+       echo "${DBUS_SESSION_BUS_ADDRESS}" > /tmp/dbus-$USER-env
+       echo "Dbus session address is: ${DBUS_SESSION_BUS_ADDRESS}"
+else
+       echo "Using dbus session address from /tmp/dbus-$USER-env"
+       export DBUS_SESSION_BUS_ADDRESS="$(cat /tmp/dbus-$USER-env)"
+       echo "Dbus session address is: ${DBUS_SESSION_BUS_ADDRESS}"
+fi
+if [ -n "$1" ]; then
+       $@
+fi
+
