07b8fe8984111cb1b8243b358ab83a885e6490c1
jhxnnat <jhonadav@tutamail.com>
2026-07-01 23:28:01 -0500

    emacs

--- private_dot_config/emacs/custom.el
+++ private_dot_config/emacs/custom.el
@@ -9,9 +9,34 @@
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-;; (menu-bar-mode -1)
+(menu-bar-mode -1)
 
 (set-face-attribute 'default nil
 		    :font "Iosevka NF"
-		    :height 140)
+		    :height 120)
 
+;; (setq indent-tabs-mode t)
+(setq-default tab-width 4
+			  inhibit-splash-screen t)
+
+(display-line-numbers-mode)
+
+;; cool shit
+(ido-mode 1)
+(ido-everywhere 1)
+
+(global-set-key (kbd "C-,") 'duplicate-line)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- private_dot_config/kmonad/executable_setup.sh
+++ private_dot_config/kmonad/executable_setup.sh
@@ -4,5 +4,9 @@ mkdir -p /etc/kmonad
 cp ./kmonad_script_external.sh /etc/kmonad/kmonad_script_external.sh
 cp ./kmonadconf-tdagger.kbd /etc/kmonad/kmonadconf-tdagger.kbd
 cp ./kmonad.service  /etc/systemd/system/kmonad.service
-cp ./kmonad /usr/local/bin/kmonad
 
+cp ./kmonad_script_laptop.sh /etc/kmonad/kmonad_script_laptop.sh
+cp ./kmonadconf-laptop.kbd /etc/kmonad/kmonadconf-laptop.kbd
+cp ./kmonadLaptop.service /etc/systemd/system/kmonadLaptop.service
+
+# cp ./kmonad /usr/local/bin/kmonad
